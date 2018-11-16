a4f016451e4f0b5512a73b46672da337c1a8e5cd
Thyion <krzbeszczynski@gmail.com>
2018-11-16 15:41:58 +0100

    lesson1

---
+++ src/main/java/pl/java/course/wsb/lesson1/Hello.java
@@ -0,0 +1,9 @@
+package pl.java.course.wsb.lesson1;
+
+public class Hello {
+
+    public static void main(String[] args){
+        System.out.println("svesvs");
+        System.out.println("dfbdfbdf");
+    }
+}
